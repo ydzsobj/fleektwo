38dd7ca8d141adbd7c4a2f750f5d58928f69833f
phpvincent@163.com <phpvincent@163.com>
2019-08-14 15:52:03 +0800

    修改接口

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -2,16 +2,16 @@ const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVu
 const LOCALURL =  "http://shop.fleekfly.xyz/"
 // const LOCALURL =  "http://192.168.1.132:8081/"
 const URL ={
-    getShopingMallInfo : BASEURL+'index',    //商城首页所有信息
+    getShopingMallInfo : LOCALURL+'api/user/index',    //商城首页所有信息
     getGoodsInfo : BASEURL+'getGoodsInfo', 
     registerUser : LOCALURL+'user/register',  //用户注册接口
     login : LOCALURL+'user/login',  //用户登录接口
 
     getDetailGoodsInfo : LOCALURL+'api/user/goods',  //获取商品详情
 
-    getCateGoryList : LOCALURL+'goods/getCateGoryList',  //得到大类信息
+    getCateGoryList : LOCALURL+'api/user/good_categories',  //得到大类信息
     getCateGorySubList : LOCALURL+'goods/getCategorySubList',  //得到小类信息
-    getGoodsListByCategorySubID : LOCALURL+'goods/getGoodsListByCategorySubID',  //得到小类商品信息
+    getGoodsListByCategorySubID : LOCALURL+'api/user/goods',  //得到小类商品信息
     sendOrderInfo: LOCALURL+'api/user/good_orders'
 }
 
