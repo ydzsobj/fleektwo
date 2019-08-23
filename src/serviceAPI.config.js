32f0525d95fb751c44d53fc22ece780eb246fc9c
fangaige <251160876@qq.com>
2019-08-23 09:31:12 +0800

    增加 cross-env ++npm run test 为测试环境

--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -1,9 +1,16 @@
-const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/"
-const LOCALURL =  "http://shop.fleekfly.xyz/"
-// const LOCALURL =  "http://192.168.1.132:8081/"
+// const BASEURL = "https://www.easy-mock.com/mock/5ae2eeb23fbbf24d8cd7f0b6/SmileVue/"
+
+if(process.env.NODE_ENV === 'development'){
+    const LOCALURL =  "http://192.168.1.132:8081/"
+}else if(process.env.NODE_ENV === 'test'){
+    const LOCALURL =  "www.website-shop.cc/"
+}else{
+    const LOCALURL =  "http://shop.fleekfly.xyz/"
+}
+console.log(process.env.NODE_ENV)
 const URL ={
     getShopingMallInfo : LOCALURL+'api/user/index',    //商城首页所有信息
-    getGoodsInfo : BASEURL+'getGoodsInfo', 
+    // getGoodsInfo : BASEURL+'getGoodsInfo', 
     registerUser : LOCALURL+'user/register',  //用户注册接口
     login : LOCALURL+'user/login',  //用户登录接口
 
